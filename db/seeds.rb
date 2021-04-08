300f6e6e87bc7021019a6d3ceff49e4db49434ba
justinhilario <justinhilario@gmail.com>
2021-04-08 14:18:39 -0700

    AR populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+apartments = [
+    {
+      street: "8169 Sterling Dr",
+      city: "El Cajon",
+      state: "CA",
+      price: 3040,
+      manager: "Clarence Thomas",
+      email: "bridgeview@gmail.com",
+      bedrooms: 4,
+      bathrooms: 2,
+      image: "https://ssl.cdn-redfin.com/photo/48/mbphoto/992/genMid.190058992_1.jpg",
+      name: "Up Above El Cajon",
+      sqft: 4208,
+      listprice: 858450,
+      hoa: 500,
+      zip: 92020,
+      user_id: 1
+    },
+     {
+      street: "1819 32nd St",
+      city: "San Diego",
+      state: "CA",
+      price: 5695,
+      manager: "Judge Judy",
+      email: "studio@yahoo.com",
+      bedrooms: 3,
+      bathrooms: 2,
+      image: "https://photos.zillowstatic.com/fp/3d578fcf09665a71c93e4c89a1d09a2b-p_e.jpg",
+      name: "ADU in back!",
+      sqft: 1520,
+      listprice: 1299000,
+      hoa: 0,
+      zip: 92102,
+      user_id: 2
+    },
+]
+
+apartments.each do |apartment|
+    Apartment.create apartment
+    puts "Created apartment: #{apartment}"
+end
