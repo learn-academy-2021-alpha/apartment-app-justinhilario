8bde4b7f1d03c9f6aed3f5805494a10f4412fbf5
justinhilario <justinhilario@gmail.com>
2021-04-08 18:18:33 -0700

    bugs

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -2,9 +2,9 @@ class ApartmentsController < ApplicationController
 
     def index
         apartments = Apartment.all 
-        render json:apartments
+        render json: apartments
     end
-
+    
     def create
         apartment = Apartment.create(apartment_params)
         render json: apartment
